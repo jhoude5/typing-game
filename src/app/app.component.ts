2ff653d05e0887bd17bdd0a6d447a29d5187b45c
Jennifer Aube <62511019+jaube05@users.noreply.github.com>
2024-02-21 10:02:41 -0500

    adding components;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { lorem } from 'faker';
+import { NgIf } from '@angular/common';
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'typinggame';
+  randomText = lorem.sentence();
+  enteredText = '';
+  onInput(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const parsedValue = String(target.value);
+  
+    this.enteredText = parsedValue;
+
+  }
+  compare(randomLetter: string, enteredLetter: string){
+    if(!enteredLetter) {
+      return 'pending';
+    }
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
